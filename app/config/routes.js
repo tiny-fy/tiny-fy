9cc34a3c6de011c126046397a5513033dbd1c908
Whitney <whitneywooyy@gmail.com>
2015-10-07 19:07:39 -0600

    Add react-router

---
+++ app/config/routes.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var Main = require('../components/Main');
+var Home = require('../components/Home');
+var Router = require('react-router');
+var IndexRoute = Router.IndexRoute;
+var Route = Router.Route;
+
+module.exports = (
+	<Route path="/" component={Main}>
+		<IndexRoute component={Home}/>
+	</Route>
+);
